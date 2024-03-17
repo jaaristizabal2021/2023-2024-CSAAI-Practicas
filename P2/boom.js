2ba4d09e2332981dbb2bb72d4cbe1fefa95595d9
joan <ja.aristizabal.2021@alumnos.urjc.es>
2024-03-17 20:56:40 +0100

    P2

--- P2/boom.js
+++ P2/boom.js
@@ -7,6 +7,20 @@ const gui = {
 
 }
 
+const botones = document.getElementsByClassName("digito")
+const m1 = document.getElementById("m1")
+const m2 = document.getElementById("m2")
+const m3 = document.getElementById("m3")
+const m4 = document.getElementById("m4")
+
+let min = 0;
+let max = 9;
+let valorM1 = Math.floor(Math.random() * (max - min + 1)) + min;
+let valorM2 = Math.floor(Math.random() * (max - min + 1)) + min;
+let valorM3 = Math.floor(Math.random() * (max - min + 1)) + min;
+let valorM4 = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(valorM1, valorM2, valorM3, valorM4);
+
 //-- Definir un objeto cronómetro
 const crono = new Crono(gui.display2);
 
@@ -27,23 +41,17 @@ gui.stop.onclick = () => {
 //-- Reset del cronómetro
 gui.reset.onclick = () => {
     console.log("Reset!");
+    m1.innerHTML = "*";
+    m2.innerHTML = "*";
+    m3.innerHTML = "*";
+    m4.innerHTML = "*";
+
     crono.reset();
 }
 
-const botones = document.getElementsByClassName("digito")
-const m1 = document.getElementById("m1")
-const m2 = document.getElementById("m2")
-const m3 = document.getElementById("m3")
-const m4 = document.getElementById("m4")
 
-let min = 0;
-let max = 9;
-let valorM1 = Math.floor(Math.random() * (max - min + 1)) + min;
-let valorM2 = Math.floor(Math.random() * (max - min + 1)) + min;
-let valorM3 = Math.floor(Math.random() * (max - min + 1)) + min;
-let valorM4 = Math.floor(Math.random() * (max - min + 1)) + min;
-console.log(valorM1, valorM2, valorM3, valorM4);
 
+m1.innerHTML = "*";
 
 
 for (let boton of botones) {
@@ -52,64 +60,30 @@ for (let boton of botones) {
   }
 }
 
-var x = 1;
 
-for (let boton of botones) {
-  boton.onclick = (ev) => {
-    if (x == 1) {
-      if (boton.value == valorM1){
-      m1.innerHTML += boton.value; 
-      }
-      console.log(x);
-      x += 1;
-
-    } else if (x == 2) {
-      if (boton.value == valorM2){
-        m2.innerHTML += boton.value; 
-        }
-      console.log(x);
-      x += 1;
-
-    } else if (x == 3) {
-      if (boton.value == valorM3){
-        m3.innerHTML += boton.value; 
-        }
-      console.log(x);
-      x += 1;
-
-    } else if (x == 4) {
-      if (boton.value == valorM4){
-        m4.innerHTML += boton.value; 
-        }
-        
-      console.log(x);
-      x = 1;
-    }
-  }
-}
 
 var x = 0;
 for (let boton of botones) {
   boton.onclick = (ev) => {
     crono.start();
     if (boton.value == valorM1){
-      m1.innerHTML += boton.value; 
+      m1.innerHTML = boton.value; 
       x += 1;
       }
 
     if (boton.value == valorM2){
-        m2.innerHTML += boton.value; 
+        m2.innerHTML = boton.value; 
         x += 1;
         }
     if (boton.value == valorM3){
-        m3.innerHTML += boton.value; 
+        m3.innerHTML = boton.value; 
         x += 1;
         }
     if (boton.value == valorM4){
-        m4.innerHTML += boton.value; 
+        m4.innerHTML = boton.value; 
         x += 1;
         }
-        if (x==4){
+    if (m4!="*" & m1!="*" & m2!="*" & m3!="*" ){
           console.log("Stop!");
           crono.stop();
         }
